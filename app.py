6480f43efae9054e3ec26f5ef316042f60ff561c
Shuvadip Ghosh <shuvadipghosh.pkt@gmail.com>
2021-07-30 07:52:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\\n\\//xec]/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyTodo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db  =  SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable =False )
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+class Ref(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    pip = db.Column(db.String(500), nullable =False )
+    link = db.Column(db.String(500), nullable =False )
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def base_site():    
+    allTodo = Todo.query.all()     
+    return render_template('index.html', todos = allTodo)
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['title']
+    desc = request.form['desc']
+    todo = Todo(title=name, desc= desc)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('base_site'))
+
+@app.route('/check' , methods=['POST'])
+def check():
+    if request.method == "POST":
+        if request.form.get('Delete'):
+            sno = request.form['Delete']
+            todo = Todo.query.filter_by(sno=sno).first()
+            db.session.delete(todo)
+            db.session.commit()
+        else :
+            sno = request.form['Update']
+            # print(sno)
+            todo = Todo.query.filter_by(sno=sno).first()
+            return render_template('update.html', todo = todo)
+    return redirect(url_for('base_site'))
+
+@app.route('/update/<int:sno>', methods=['POST'])
+def update(sno):
+    todo = Todo.query.get_or_404(sno)
+    if request.method == "POST":
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        try:
+            db.session.commit()
+            return redirect(url_for('base_site'))
+        except:
+            return render_template('errorMessage.html')
+
+@app.route('/reference', methods=['GET', 'POST'])
+def reference():
+    if request.method == "POST":
+        name = request.form['title']
+        desc = request.form['desc']
+        link = request.form['link']
+        referenc = Ref(title=name, pip = desc, link = link)
+        db.session.add(referenc)
+        db.session.commit()
+        flash("Added your reference successfully")
+        ref = Ref.query.all()
+        return render_template('reference.html',refs = ref)
+    ref = Ref.query.all()
+    return render_template('reference.html',refs = ref)
+
+@app.route('/copy', methods=['GET', 'POST'])
+def copy():
+    if request.method == "POST":
+        if request.form.get('copy'):
+            var = request.form.get('copy')
+            print (var)
+            import clipboard
+            clipboard.copy(var)
+            flash("Copied")
+            return redirect('/reference')
+        elif request.form.get('Delete'):
+            sno = request.form['Delete']
+            todo = Ref.query.filter_by(sno=sno).first()
+            db.session.delete(todo)
+            db.session.commit()
+            flash("Reference Deleted successfully")
+            return redirect('/reference')
+
+@app.route('/ref', methods=['GET', 'POST'])
+def ref():
+    if request.method == 'POST':
+        return redirect(url_for('reference'))
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        if request.form.get('reference'):
+            return redirect(url_for('reference'))
+        search = request.form['search']
+        tems = Ref.query.filter((Ref.title.contains(search)) | (Ref.pip.contains(search))).all()
+        return render_template('reference.html',refs = tems)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
